375e84f179acf6e73683abec35b83d98b615bf49
Tobi <adegbuji_tobi@yahoo.com>
2020-12-10 09:05:11 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Login from './component/login/Login'; 
 import SignUp from './component/signup/SignUp';
+import RouterComponent from './component/general/RouterComponent'
 
 function App() {
   return (
     <div className="App"> 
-      {/* <Login/> */}
-      <SignUp/>
+      <RouterComponent/>
     </div>
   );
 }
--- src/component/login/Login.jsx
+++ src/component/login/Login.jsx
@@ -1,5 +1,7 @@
 import './Login.css';
+import {Link} from 'react-router-dom'
 import React from 'react'; 
+import image from '../../img/leaf.jpg'
 
 
 class Login extends React.Component{
@@ -13,22 +15,25 @@ class Login extends React.Component{
         return(
             <div className="loginContainer card p-5">
 
-                <h1>Login</h1>
+                    <h1>Login</h1>
+                    <form onSubmit={this.handleSubmition}>
 
-                <form action="">
-                    <label>
-                        <input className="m-3" type="text" placeholder="username"></input>
-                    </label>
-                    <div className="m-3">
-                    <label>
-                        <input type="password" placeholder="password"></input>
-                    </label>
+                    <div className="form-group">
+                    <label>Username</label>
+                    <input className="form-control" name="userName" type="text" placeholder="username" onChange={this.handleChange}></input>
                     </div>
-                    <input className="btn btn-primary m-3" type="submit" value="Login"/>
-                </form>
-                <button className="btn btn-success signUpBtn mx-auto">Sign Up</button>
 
-            </div>
+                    <div className="form-group">
+                    <label>Password</label>
+                    <input className="form-control" name="password" type="password" placeholder="password" onChange={this.handleChange}></input>
+                    </div>
+
+                        <input className="btn btn-primary m-3" type="submit" value="Login"/>
+                    
+                    </form>
+                    <Link className="btn btn-success signUpBtn mx-auto" to="/signup">Sign Up</Link>
+                </div>
+                
         );
     }
 
